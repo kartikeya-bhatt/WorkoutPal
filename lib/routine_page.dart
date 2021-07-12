41251c1fafd8ee220e895738d8a4d6f2be1a9b37
kartikeya-bhatt <krtky020@gmail.com>
2021-07-12 02:48:24 -0700

    Made workout name editable, does not save entries yet

--- lib/routine_page.dart
+++ lib/routine_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'package:flutter/services.dart';
+import 'package:auto_size_text_field/auto_size_text_field.dart';
+
 
 class RoutinePage extends StatelessWidget {
   final String day;
@@ -18,18 +21,33 @@ class RoutinePage extends StatelessWidget {
   }
 }
 
-class DayHeader extends StatelessWidget {
+class DayHeader extends StatefulWidget {
   final String day;
   DayHeader(this.day);
 
+  @override
+  _DayHeaderState createState() => _DayHeaderState();
+}
+
+class _DayHeaderState extends State<DayHeader> {
+  final myController = TextEditingController();
+  String _save_text;
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
+    bool isDefault = _save_text == null;
     return Container(
       child: Column(
         children: [
           RichText(
             textAlign: TextAlign.center,
             text: TextSpan(
-              text: day,
+              text: widget.day,
               style: TextStyle(
                 fontSize: 60,
                 color: white,
@@ -37,7 +55,23 @@ class DayHeader extends StatelessWidget {
               ),
             ),
           ),
-          Text("Push Day",
+          AutoSizeTextField(
+              controller: isDefault ? TextEditingController(text: "Enter Workout Name") : TextEditingController(text: _save_text),
+              maxLines: 1,
+              cursorColor: white,
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(30),
+              ],
+              decoration: new InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+              ),
+              onChanged: (text) {
+                _save_text = text;
+                print(text);
+              },
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 24,
@@ -49,3 +83,4 @@ class DayHeader extends StatelessWidget {
     );
   }
 }
+
