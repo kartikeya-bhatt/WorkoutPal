17e456b031b465d817006011aff8710a03ba490d
kartikeya-bhatt <krtky020@gmail.com>
2021-07-19 21:57:26 -0700

    Fixed bug with login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,11 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:frontend/home_page.dart';
 import 'package:frontend/routine_page.dart';
 import 'package:frontend/make_account.dart';
+import 'User.dart';
 import 'constants.dart';
 
-
-
 class CreateAccount extends StatelessWidget {
+
+  List<Day> days = [];
+  final List<String> dayNames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  User myUser;
+
+  CreateAccount () {
+    for(String name in dayNames) {
+      List<Exercise> exercises = [];
+      days.add(new Day(name, exercises));
+    }
+
+    myUser = new User("", "", days);
+  }
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
@@ -15,17 +37,16 @@ class CreateAccount extends StatelessWidget {
     return SafeArea(
       child: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                Color(0xFF73AEF5),
-                  Color(0xFF61A4F1),
-                  Color(0xFF478DE0),
-                  Color(0xFF398AE5),
-                ],
-            )
-          ),
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF73AEF5),
+              Color(0xFF61A4F1),
+              Color(0xFF478DE0),
+              Color(0xFF398AE5),
+            ],
+          )),
           alignment: Alignment.center,
           child: SingleChildScrollView(
             child: Column(
@@ -35,72 +56,77 @@ class CreateAccount extends StatelessWidget {
                   SizedBox(height: h * 0.07),
                   Container(
                       alignment: Alignment.centerLeft,
-                      constraints: BoxConstraints.tightFor(width: w * 0.875, height: h * 0.025),
-                      child: Text('Username',
-                      style: TextStyle(
+                      constraints: BoxConstraints.tightFor(
+                          width: w * 0.875, height: h * 0.025),
+                      child: Text(
+                        'Username',
+                        style: TextStyle(
                           color: Colors.white,
                           fontSize: 15.0,
                           fontFamily: 'Ubuntu',
                           fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ),
+                        ),
+                      )),
                   SizedBox(height: h * 0.01),
                   Container(
-                      constraints: BoxConstraints.tightFor(width: w * 0.9, height: h * 0.075),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF6CA8F1),
-                        borderRadius: BorderRadius.circular(10.0),
-                        boxShadow: [
-                              BoxShadow(
-                              color: Colors.black26,
-                              blurRadius: 10.0,
-                              offset: Offset(0, 2),
-                            ),
-                          ],
+                    constraints: BoxConstraints.tightFor(
+                        width: w * 0.9, height: h * 0.075),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF6CA8F1),
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10.0,
+                          offset: Offset(0, 2),
                         ),
-                        alignment: Alignment.centerLeft,
-                        child: SingleChildScrollView(
-                          child: TextFormField(
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.white70,
-                              fontFamily: 'Ubuntu',
-                              fontWeight: FontWeight.w400,
-                            ),
-                            decoration: const InputDecoration(
-                              contentPadding: EdgeInsets.only(top: 16),
-                              border: InputBorder.none,
-                              prefixIcon: Icon(Icons.person,
-                                  color: Colors.white,
-                              ),
-                              hintText: 'Enter your username',
-                              hintStyle: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white70,
-                                fontFamily: 'Ubuntu',
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
+                      ],
+                    ),
+                    alignment: Alignment.centerLeft,
+                    child: SingleChildScrollView(
+                      child: TextFormField(
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white70,
+                          fontFamily: 'Ubuntu',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(top: 16),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          hintText: 'Enter your username',
+                          hintStyle: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white70,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
+                      ),
                     ),
+                  ),
                   SizedBox(height: h * 0.025),
                   Container(
                       alignment: Alignment.centerLeft,
-                      constraints: BoxConstraints.tightFor(width: w * 0.875, height: h * 0.025),
-                      child: Text('Password',
+                      constraints: BoxConstraints.tightFor(
+                          width: w * 0.875, height: h * 0.025),
+                      child: Text(
+                        'Password',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 15,
                           fontFamily: 'Ubuntu',
                           fontWeight: FontWeight.bold,
                         ),
-                      )
-                  ),
+                      )),
                   SizedBox(height: h * 0.01),
                   Container(
-                    constraints: BoxConstraints.tightFor(width: w * 0.9, height: h * 0.075),
+                    constraints: BoxConstraints.tightFor(
+                        width: w * 0.9, height: h * 0.075),
                     decoration: BoxDecoration(
                       color: Color(0xFF6CA8F1),
                       borderRadius: BorderRadius.circular(10.0),
@@ -124,8 +150,7 @@ class CreateAccount extends StatelessWidget {
                       decoration: const InputDecoration(
                         border: InputBorder.none,
                         contentPadding: EdgeInsets.only(top: 17),
-                        prefixIcon: Icon(Icons.lock,
-                        color: Colors.white),
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
                         hintText: 'Enter your password',
                         hintStyle: TextStyle(
                           fontSize: 15,
@@ -138,7 +163,8 @@ class CreateAccount extends StatelessWidget {
                   ),
                   SizedBox(height: h * .025),
                   Container(
-                    child: Text('Error: Username and Password do not match',
+                    child: Text(
+                      'Error: Username and Password do not match',
                       style: TextStyle(
                         fontFamily: 'Ubuntu',
                         fontWeight: FontWeight.bold,
@@ -149,7 +175,8 @@ class CreateAccount extends StatelessWidget {
                   ),
                   SizedBox(height: h * 0.05),
                   Container(
-                    constraints: BoxConstraints.tightFor(width: w * 0.6, height: h * 0.05),
+                    constraints: BoxConstraints.tightFor(
+                        width: w * 0.6, height: h * 0.05),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10.0),
                       boxShadow: [
@@ -164,24 +191,28 @@ class CreateAccount extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => HomePage()),
+                          MaterialPageRoute(builder: (context) => HomePage(myUser)),
                         );
-                        },
-                      child: Text('SUBMIT',
-                        style: TextStyle(fontSize: 20,
+                      },
+                      child: Text(
+                        'SUBMIT',
+                        style: TextStyle(
+                          fontSize: 20,
                           fontFamily: 'OpenSans',
                           letterSpacing: 1.6,
                           color: Color(0xFF478DE0),
                           fontWeight: FontWeight.w300,
                         ),
                       ),
-                      style: ElevatedButton.styleFrom(shape: StadiumBorder(), primary: white, // background
-                        ),
+                      style: ElevatedButton.styleFrom(
+                        shape: StadiumBorder(), primary: white, // background
+                      ),
                     ),
                   ),
                   SizedBox(height: h * 0.035),
                   Container(
-                    child: Text('- OR -',
+                    child: Text(
+                      '- OR -',
                       style: TextStyle(
                         fontFamily: 'Ubuntu',
                         fontWeight: FontWeight.w300,
@@ -198,19 +229,18 @@ class CreateAccount extends StatelessWidget {
                         MaterialPageRoute(builder: (context) => MakeAccount()),
                       );
                     },
-                    child: Text('Create an account',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontFamily: 'OpenSans',
-                          color: white,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    child: Text(
+                      'Create an account',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'OpenSans',
+                        color: white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                ]
-            ),
-          )
-      ),
+                  ),
+                ]),
+          )),
     );
   }
 }
